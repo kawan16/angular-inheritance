b798ae6eabf3718fc8601a00fd8f5cd22f93fd38
kawan16 <karl.devooght@gmail.com>
2014-09-14 22:37:04 +0200

    Add  fruit crud example

--- example/crud/fruit/fruitCrudsController.js
+++ example/crud/fruit/fruitCrudsController.js
@@ -4,12 +4,29 @@
  * Date :   13/09/2014
  */
 
-( function( ng ) {
+( function( ng , appName ) {
 
-    function FruitCrudsController( $scope , FruitCrudsService ) {
-        this.super( 'CrudsController' ).apply( arguments );
+    function FruitCrudsController( $scope , FruitCrudsService , $modal ) {
+        this.super( 'CrudsController' ,  [ $scope, FruitCrudsService ]);
+        this.super( 'ModalController' ,  [ $scope, $modal ]);
+
+        console.log('fruit cruds open modal ' + this.openModal);
+
+        // Initial search
+        this.filter( "" );
+        this.search();
     }
 
-    ng.inherit().controller( FruitCrudsController , 'CrudsController');
+    ng.expand().controller( FruitCrudsController , [ 'CrudsController' , 'ModalController' ] );
 
-}) ( angular );
+
+    // Override
+    FruitCrudsController.prototype.doAfterSuccessSearch = function( result ) {
+        this.parent('CrudsController').doAfterSuccessSearch.apply( this , [ result ]  );
+        // Need to scope apply since we don't use real xhr requests
+        this.scope.$apply();
+    };
+
+    ng.module( appName ).controller( 'FruitCrudsController' , FruitCrudsController );
+
+}) ( angular , 'CrudDemo');
--- example/crud/fruit/fruitCrudsService.js
+++ example/crud/fruit/fruitCrudsService.js
@@ -1,17 +1,37 @@
 
-( function( ng ) {
+( function( ng , appName ) {
 
-    function FruitCrudsService() {
-        this.super( 'Crudsservice' ).apply( arguments );
+    function FruitCrudsService( ) {
+        this.super( 'CrudsService' ,  arguments );
 
         this.fruits = [
-
+            {
+                name:"Apple" ,
+                designation:"The apple is the pomaceous fruit of the apple tree, Malus domestica of the rose family (Rosaceae)."
+            },
+            {
+                name:"Banana" ,
+                designation:"A banana is an edible fruit, botanically a berry, produced by several kinds of large herbaceous flowering plants in the genus Musa."
+            },
+            {
+                name:"Orange" ,
+                designation:"The orange (specifically, the sweet orange) is the fruit of the citrus species Citrus x sinensis in the family Rutaceae."
+            }
         ];
     }
 
     ng.inherit().service( FruitCrudsService , 'CrudsService');
 
+    // Overriding
+    FruitCrudsService.prototype.search = function( filter ) {
+        this.log.info('FruitsCrudsService is searching items ' );
+        var defer = this.q.defer();
+        var service = ng.injector(['ng']).get('$filter');
+        defer.resolve( service('filter')( this.fruits , { name: filter } ) );
+        return defer.promise;
+    };
+
+    ng.module( appName ).service( 'FruitCrudsService' , FruitCrudsService );
 
 
-
-}) ( angular );
+}) ( angular , 'CrudDemo');
